9c49ea30dd4a3c329a0704a1c1726243eeddfcaa
aikhacode <aikhacomp@gmail.com>
2022-07-12 02:32:18 +0700

    commit surat keluar masuk2

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,10 @@
 <?php
 
-use App\Http\Controllers\PegawaiController;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Storage;
 use App\Models\Arsip;
 use App\Models\SuratMasuk;
-use Illuminate\Http\Request;
-
 use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,55 +17,49 @@ use Barryvdh\DomPDF\Facade\Pdf;
 |
  */
 
-
 Route::get('/', function () {
 	return redirect('/app');
 	// return view('welcome');
 });
 
-Route::get('upload/arsip', function (Request $request)
-{
+Route::get('upload/arsip', function (Request $request) {
 	$storagepath = base64_decode($request->input('arsip'));
 	// return $storagepath;
 
-    $arsip = Arsip::where('storagepath',$storagepath)->get();
+	$arsip = Arsip::where('storagepath', $storagepath)->get();
 
-    // return response($arsip);
-    if ($arsip)
-    {
-    	 $path = public_path($storagepath);
-		       	        
-       
-	    if (!File::exists($path)) {
-	        abort(404);
-	    
-	    }
+	// return response($arsip);
+	if ($arsip) {
+		$path = storage_path($storagepath);
 
-	    return response()->download($path,$arsip[0]->filename);
-	    // return response()->file($path);
-    } else {
-    	abort(404);
+		if (!File::exists($path)) {
+			abort(404);
 
-    }
+		}
 
-   
-   
+		return response()->download($path, $arsip[0]->filename);
+		// return response()->file($path);
+	} else {
+		abort(404);
+
+	}
+
 });
 
-Route::get('/print/disposisi/{id}', function ($id){
+Route::get('/print/disposisi/{id}', function ($id) {
 	$surat = SuratMasuk::find($id);
 	$surat->disposisis;
 
-	$pdf = Pdf::loadview('disposisi',['data'=> $surat->disposisis]);
+	$pdf = Pdf::loadview('disposisi', ['data' => $surat->disposisis]);
 	return $pdf->stream();
 	// $pdf = App::make('dompdf.wrapper');
-    // $pdf->loadHTML('<h1>Test</h1>');
-    // return $pdf->stream();
+	// $pdf->loadHTML('<h1>Test</h1>');
+	// return $pdf->stream();
 });
 
-Route::get('/preview/disposisi/{id}', function ($id){
+Route::get('/preview/disposisi/{id}', function ($id) {
 	$surat = SuratMasuk::find($id);
 	$surat->disposisis;
 
-	return view('disposisi',['data'=>$surat->disposisis]);
+	return view('disposisi', ['data' => $surat->disposisis]);
 });
